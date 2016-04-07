f6238f31de3b4b706416b609ae4be6c36539c5e8
A.H <Akihiro@thomasdeMacBook-Pro.local>
2016-04-07 17:20:51 +0800

    DTREE

--- DTREE.R
+++ DTREE.R
@@ -1,5 +1,7 @@
 #資料挖礦與大數據分析==========
 
+#CART
+
 library(MASS)
 library(rpart) #CART決策樹
 data("Pima.tr")
@@ -23,6 +25,8 @@ accuracy <- sum(diag(confusion_matrix)) / sum(confusion_matrix)
 accuracy
 
 
+#C5.0
+
 install.packages("C50")
 library(C50) #C5.0決策樹
 library(MASS)
@@ -30,6 +34,9 @@ data("Pima.tr")
 C50_tree <- C5.0(type ~., Pima.tr, control = C5.0Control(noGlobalPruning = T)) #F則會修剪樹
 summary(C50_tree)
 plot(C50_tree)
+
+
+#CHAID
 
 install.packages("CHAID", repos="http://R-Forge.R-project.org", type="source")
 library(CHAID)
@@ -56,6 +63,7 @@ plot(CHAID_tree)
 
 #利用R語言打通大數據的經脈==========
 
+##資料準備==========
 
 rpart(formula, data, weights, subset, na.action = na.rpart, method, model = FALSE, x = FALSE, y = TRUE, parms, control, cost, ...) #method參數用於選擇決策樹的類型，包含anova、poisson、class、exp四種，不進行設定的預設情況下，R會自行猜測，例如當y為因數型變數時，預設為class型，其中，anova對應的是回歸樹，class則是分類樹
 rpart.control(minsplit = 20, minbucket = round(minsplit/3), cp = .01, maxcompete = 4, maxsurrogate = 5, usesurrogate = 2, xval = 10, surrogatestyle = 0, maxdepth = 30) #minsplit為每個節點最少需要幾個樣本，minbucket為每個葉節點最少需要幾個樣本，cp為複雜度參數，maxdepth控制樹的高度
@@ -68,7 +76,7 @@ str(car.test.frame)
 summary(car.test.frame)
 
 Group_Mileage <- matrix(0, 60, 1)
-Group_Mileage[which(car.test.frame$Mileage >= 26)] = "A"
+Group_Mileage[which(car.test.frame$Mileage >= 26)] = "A" #連續變數轉為類別變數
 Group_Mileage[which(car.test.frame$Mileage <= 22)] = "C"
 Group_Mileage[which(Group_Mileage == 0)] = "B"
 
@@ -81,7 +89,8 @@ c <- round(1/4 * sum(car.test.frame$Group_Mileage == "C"))
 a; b; c
 
 library(sampling)
-sub <- strata(car.test.frame, stratanames = "Group_Mileage", size = c(c, b, a), method = "srswor") #用strata()進行分層抽樣
+sub <- strata(car.test.frame, stratanames = "Group_Mileage", 
+              size = c(c, b, a), method = "srswor") #用strata()進行分層抽樣
 sub
 
 Train_Car <- car.test.frame[-sub$ID_unit, ]
@@ -90,16 +99,20 @@ nrow(Train_Car)
 nrow(Test_Car)
 
 
+##CART應用==========
+
+###建立迴歸樹
+
 library(rpart)
 formula_Car_Reg <- Mileage ~ Price + Country + Reliability + Type + Weight + Disp. + HP
 rp_Car_Reg <- rpart(formula_Car_Reg, Train_Car, method = "anova")
 print(rp_Car_Reg)
 
-printcp(rp_Car_Reg) #匯出回歸樹的CP表格
+printcp(rp_Car_Reg) #匯出回歸樹的CP表格用printcp()
 
 summary(rp_Car_Reg)
 
-rp_Car_Reg1 <- rpart(formula_Car_Reg, Train_Car, method = "anova", minsplit = 10)
+rp_Car_Reg1 <- rpart(formula_Car_Reg, Train_Car, method = "anova", minsplit = 10) #anova為迴歸樹
 print(rp_Car_Reg1)
 printcp(rp_Car_Reg1)
 
@@ -107,20 +120,20 @@ rp_Car_Reg2 <- rpart(formula_Car_Reg, Train_Car, method = "anova", cp = .1)
 print(rp_Car_Reg2)
 printcp(rp_Car_Reg2)
 
-rp_Car_Reg3 <- prune.rpart(rp_Car_Reg, cp = .1)
+rp_Car_Reg3 <- prune.rpart(rp_Car_Reg, cp = .1) #用prune.rpart達到相同較果
 print(rp_Car_Reg3)
 printcp(rp_Car_Reg3)
 
-rp_Car_Reg4 <- rpart(formula_Car_Reg, Train_Car, method = "anova", maxdepth = 1)
+rp_Car_Reg4 <- rpart(formula_Car_Reg, Train_Car, method = "anova", maxdepth = 1) #也可用maxdepth控制樹的大小（事前修剪）
 print(rp_Car_Reg4)
 printcp(rp_Car_Reg4)
 
 
-install.packages("rpart.plot")
+install.packages("rpart.plot") #繪製決策樹
 library(rpart.plot)
 rp_Car_Plot <- rpart(formula_Car_Reg, Train_Car, method = "anova", minsplit = 10)
 print(rp_Car_Plot)
-rpart.plot(rp_Car_Plot) #繪製決策樹
+rpart.plot(rp_Car_Plot)
 
 rpart.plot(rp_Car_Plot, type = 4) #更改決策樹類型
 rpart.plot(rp_Car_Plot, type = 4, branch = 1) #branch
@@ -136,11 +149,16 @@ text(rp_Car_Plot, use.n = TRUE, all = TRUE)
 post(rp_Car_Plot, file = "") #post()也可以畫決策樹
 
 
+###建立分類樹
+
 formula_Car_Cla <- Group_Mileage ~ Price + Country + Reliability + Type + Weight + Disp. + HP
 rp_Car_Cla <- rpart(formula_Car_Cla, Train_Car, method = "class", minsplit = 5)
 print(rp_Car_Cla)
 
-rpart.plot(rp_Car_Cla, type = 4, fallen.leaves = TRUE)
+rpart.plot(rp_Car_Cla, type = 4, fallen.leaves = TRUE) #繪製分類樹
+
+
+###進行預測
 
 pre_Car_Cla <- predict(rp_Car_Cla, Test_Car, type = "class")
 pre_Car_Cla
@@ -148,6 +166,8 @@ p <- sum(as.numeric(pre_Car_Cla != Test_Car$Group_Mileage)) / nrow(Test_Car)
 p
 table(Test_Car$Group_Mileage, pre_Car_Cla)
 
+
+##C4.5應用==========
 
 install.packages("RWeka")
 library(RWeka)
