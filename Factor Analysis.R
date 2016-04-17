957ce00e27d3fc508c19fc091f858e10d60e80fa
Toma-L <anthony41213@hotmail.com>
2016-04-17 17:17:52 +0800

    +Factor Analysis

--- Factor Analysis.R
+++ Factor Analysis.R
@@ -1,4 +1,4 @@
-#Factor Analysis
+#因素分析與信度分析==============================
 
 r.no <- factanal(sca[-c(3, 10, 13)], factors = 2, data = sca, rotation = "none")
 r.no$loadings
@@ -22,7 +22,6 @@ grid(nx = 10, ny = 10)
 #斜交轉軸法：最優斜交法/直接斜交法/簡單最大法
 #因素分數型態：none/regression(Thompson分數)/Barlett加權最小平方分數
 #要輸出何種因素矩陣：結構矩陣/樣式矩陣（組型矩陣）
-
 
 ##基本套件factanal()函數的應用==============================
 
@@ -161,7 +160,7 @@ plot(eig, pch = 16, cex = 1.5) #繪製陡坡圖
 lines(eig, col = 4, lwd = 2)
 grid(nx = 17, ny = 10)
 
-pr4.no <- principal(efa, 4, rotate = "none") #主成份法+不轉軸
+pr4.no <- principal(efa, 4, rotate = "none") #主成份＋不轉軸
 print(pr4.no, digits = 2)
 
 
@@ -177,3 +176,166 @@ print(pc.m, cut = .40, sort = TRUE) #因素負荷量小於.40的不會呈現
 
 efa.1 <- efa[, -2]
 names(efa.1)
+
+pc.m1 <- principal(efa.1, 3, rotate = "varimax") #主成份＋最大變異
+print(pc.m1, sort = TRUE) #DC16為跨因素效度的指標變數，下一步刪除之
+
+
+###刪除第二題、第十六題（DA2、DA16）的EFA
+
+efa.2 <- efa[, -c(2, 16)]
+pc.m2 <- principal(efa.2, 3, rotate = "varimax")
+print(pc.m2, cut = .40, sort = TRUE)
+
+
+###刪除DA2、DB6、DC16的EFA
+
+efa.3 <- efa[, -c(2, 6, 16)]
+pc.m3 <- principal(efa.3, 3, rotate = "varimax")
+print(pc.m3, cut = 0, sort = TRUE)
+
+round(pc.m3$loadings[, 2], 2)
+print(pc.m3, cut = 0, sort = TRUE)
+print(pc.m3, cut = .45, sort = FALSE)
+
+fa.diagram(pc.m3, digits = 2) #繪製直交轉軸的因素構念圖！
+
+efa.3 <- efa[, -c(2, 6, 16)]
+pc.p3 <- principal(efa.3, 3, rotate = "promax")
+print(pc.p3)
+
+fa.diagram(pc.p3, digits = 2) #斜交轉軸因素構念圖
+
+
+##fa()函數的應用==============================
+
+fam <- fa(efa, nfactors = 4, rotate = "varimax", fm = "pa") #主軸法＋最大變異
+print(fam, sort = TRUE)
+
+names(fam)
+
+efa.3 <- efa[, -c(2, 6, 16)] #主軸法＋最大變異
+fam <- fa(efa.3, nfactors = 3, rotate = "varimax", fm = "pa")
+print(fam, sort = TRUE) #DC14跨因素效度，刪除之
+
+
+efa.4 <- efa[, -c(2, 6, 14, 16)]
+fam.p <- fa(efa.4, nfactors = 3, rotate = "oblimin", oblique.scores = FALSE, fm = "pa")
+print(fam.p, sort = TRUE)
+
+
+fam.p <- fac(efa.4, nfactors = 3, rotate = "oblimin", oblique.scores = FALSE, fm = "pa") #fac()的語法與函數fa()類似
+
+library(psych)
+library(GPArotation)
+
+pc <- principal(efa.3, 3, rotate = "none")
+fa <- fa(efa.3, 3, rotate = "none")
+
+factor.congruence(fa, pc) #factor.congruence()可以進行不同共同因素分析萃取方法之因素結構一致性的檢核
+#最小殘差法與主成份分析法未轉軸之因素結構比較，一致性高
+
+pcv <- principal(efa.3, 3, rotate = "varimax")
+fav <- fa(efa.3, 3, rotate = "varimax")
+factor.congruence(pcv, fav)
+
+pcv <- principal(efa.3, 3, rotate = "varimax")
+pav <- fa(efa.3, 3, rotate = "varimax", fm = "pa")
+factor.congruence(pav, pcv)
+
+
+##信度分析==============================
+
+###使用套件psych中的函數alpha()
+
+library(psych)
+temp <- read.csv("efa_1.csv", header = TRUE)
+names(temp)
+
+####求出外向性構面的信度
+
+fact1 <- efa[c(1, 3, 4, 5)]
+names(fact1)
+alpha(fact1) 
+
+#Reliability if an item is dropped是刪除該題項後，剩餘題項的信度變化
+#Item statistics的raw.r為題項與因素構面總分的相關係數
+#Non missing response frequency for each item為題項變數遺漏值個數
+
+factsum <- rowSums(fact1) #因素構面總分
+round(cor(fact1, factsum), 2) #觀察值與因素構面總分的相關
+
+cor(fact1[, 1], rowSums(fact1[-1])) #修正的項目總相關
+cor(fact1[, 2], rowSums(fact1[-2])) #即Item statistics的r.drop
+cor(fact1[, 3], rowSums(fact1[-3]))
+cor(fact1[, 4], rowSums(fact1[-4]))
+
+####求出經驗開放性構面的信度
+
+fact2 <- efa[c(7:11)]
+names(fact2)
+alpha(fact2)
+
+####求出宜人性構面的信度
+
+fact3 <- efa[c(12:15, 17)]
+names(fact3)
+alpha(fact3)
+
+####人格特質十四題的信度
+
+totscale <- efa[-c(2, 6, 16)]
+names(totscale)
+alpha(totscale)
+
+
+###使用函數splitHalf()求折半信度
+
+####外向性因素構面的折半信度
+
+splitHalf(fact1)
+#看Guttman lambda 6為可靠統計量
+#看Guttman lambda 3 (alpha)，就是Cronbach alpha
+#看Minimum split half reliability (beta)
+
+####經驗開放性因素構面的折半信度
+
+splitHalf(fact2)
+
+####宜人性因素構面的折半信度
+
+splitHalf(fact3)
+
+####人格特質總量表的折半信度
+
+splitHalf(totscale)
+
+
+###使用函數splithalf.r()求信度
+
+####求出外向性因素構念的信度
+
+install.packages("multicon")
+library(multicon)
+
+fact1 <- efa[c(1, 3, 4, 5)]
+splithalf.r(fact1, graph = F)
+
+#兩折半間相關係數為0.733
+#信度估計值alpha為0.846
+#折半信度估計值標準誤0.048
+
+####求出經驗開放性因素構念的信度
+
+fact2 <- efa[c(7:11)]
+splithalf.r(fact2, graph = FALSE)
+
+####求出宜人性因素構念的信度
+
+fact3 <- efa[c(12:15, 17)]
+splithalf.r(fact3, graph = FALSE)
+
+####求出整體人格特質量表的信度
+
+totscale <- efa[-c(2, 6, 16)]
+splithalf.r(totscale, graph = FALSE)
