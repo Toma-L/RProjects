d7c697f77671734b571876bc24fe70c20c593eb4
Toma-L <anthony41213@hotmail.com>
2016-04-29 18:27:57 +0800

    +Unsupervised Learning

--- PRACML.R
+++ PRACML.R
@@ -587,3 +587,100 @@ qplot(predict(modFit, testing), wage, data = testing)
 
 #假設資料follow某機率模型，用貝氏機率去指認出來
 
+data(iris)
+library(ggplot2)
+names(iris)
+table(iris$Species)
+
+inTrain <- createDataPartition(y = iris$Species, p = .7, list = FALSE)
+training <- iris[inTrain, ]
+testing <- iris[-inTrain, ]
+dim(training); dim(testing)
+
+modlda <- train(Species ~., data = training, method = "lda") #LDA
+modnb <- train(Species ~., data = training, method = "nb") #Naive Bayes
+plda <- predict(modlda, testing)
+pnb <- predict(modnb, testing)
+table(plda, pnb)
+
+equalPredictions <- (plda == pnb)
+qplot(Petal.Width, Sepal.Width, colour = equalPredictions, data = testing)
+
+
+#Regularized Regression==================================================
+
+library(ElemStatLearn)
+data(prostate)
+str(prostate)
+
+small <- prostate[1:5, ]
+lm(lpsa ~., data = small)
+
+#in caret methods are:
+##ridge
+##lasso
+##relaxo
+
+
+#Combining predictors==================================================
+
+library(ISLR)
+data(Wage)
+library(ggplot2)
+library(caret)
+Wage <- subset(Wage, select = -c(logwage))
+
+inBuild <- createDataPartition(y = Wage$wage, p = .7, list = FALSE)
+validation <- Wage[-inBuild, ]
+buildData <- Wage[inBuild, ]
+inTrain <- createDataPartition(y = buildData$wage, p = .7, list = FALSE)
+training <- buildData[inTrain, ]
+testing <- buildData[-inTrain, ]
+
+dim(training); dim(testing); dim(validation)
+
+mod1 <- train(wage ~., method = "glm", data = training)
+mod2 <- train(wage ~., method = "rf", data = training, trControl = trainControl(method = "cv"), number = 3)
+
+pred1 <- predict(mod1, testing)
+pred2 <- predict(mod2, testing)
+
+qplot(pred1, pred2, colour = wage, data = testing)
+
+predDF <- data.frame(pred1, pred2, wage = testing$wage)
+combModFit <- train(wage ~ ., method = "gam", data = predDF)
+combPred <- predict(combModFit, predDF)
+
+sqrt(sum((pred1 - testing$wage) ^ 2))
+sqrt(sum((pred2 - testing$wage) ^2 ))
+sqrt(sum((combPred - testing$wage) ^ 2))
+
+pred1V <- predict(mod1, validation)
+pred2V <- predict(mod2, validation)
+predVDF <- data.frame(pred1 = pred1V, pred2 = pred2V)
+combPredV <- predict(combModFit, predVDF)
+
+sqrt(sum((pred1V - validation$wage) ^ 2))
+sqrt(sum((pred2V - validation$wage) ^ 2))
+sqrt(sum((combPredV - validation$wage) ^ 2))
+
+
+#Unsupervised prediction==================================================
+
+data(iris)
+library(ggplot2)
+inTrain <- createDataPartition(y = iris$Species, p = .7, list = FALSE)
+training <- iris[inTrain, ]
+testing <- iris[-inTrain, ]
+dim(training); dim(testing)
+
+kMeans1 <- kmeans(subset(training, select = -c(Species)), centers = 3)
+training$clusters <- as.factor(kMeans1$cluster)
+qplot(Petal.Width, Petal.Length, colour = clusters, data = training)
+
+table(kMeans1$cluster, training$Species)
+modFit <- train(clusters ~., data = subset(training, select = -c(Species)), method = "rpart")
+table(predict(modFit, training), training$Species)
+
+testClusterPred <- predict(modFit, testing)
+table(testClusterPred, testing$Species)
